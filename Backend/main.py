db09e88f757e65fca4102998200a9e8cc6094bc8
Florian Yun <yunflorian@hotmail.fr>
2023-11-08 00:27:26 +0100

    Added put request

--- Backend/main.py
+++ Backend/main.py
@@ -44,14 +44,15 @@ class Element(BaseModel):
     titre : str
     groupe : str
     source : str
+    ordre : int
 
 @app.get("/elements/")
 async def get_elements():
     conn = sqlite3.connect('tier_list.db')
     cursor = conn.cursor()
     try:
-        cursor.execute("SELECT id, titre, groupe, source FROM element WHERE category_id IS NULL")
-        elements = [{"id": row[0], "titre": row[1], "groupe": row[2], "source": row[3]} for row in cursor.fetchall()]
+        cursor.execute("SELECT id, titre, groupe, source FROM element WHERE category_id IS NULL ORDER BY ordre")
+        elements = [{"id": row[0], "titre": row[1], "groupe": row[2], "source": row[3], "ordre": row[4]} for row in cursor.fetchall()]
         return elements
     except Exception as e:
         logging
@@ -79,6 +80,19 @@ async def delete_element(id: int):
     cursor = conn.cursor()
     try:
         cursor.execute("DELETE FROM element WHERE id=?", (id,))
+        conn.commit()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.put("/elements/{id}")
+async def delete_element(element: Element):
+    conn = sqlite3.connect('tier_list.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute("UPDATE element SET titre=? groupe=? source=? ordre=? WHERE id=?", (element.titre, element.groupe, element.source, element.ordre, element.id))
         conn.commit()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
